c1a14a4376c6351ddfb99ee579836e198d3a4bda
Nicholas Licalsi <licalsinj@gmail.com>
2026-08-04 14:51:00 -0500

    changed default ports and started building out the excel file's power query

--- api/run.sh
+++ api/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# Starts the FastAPI development server on port 8000.
+# Starts the FastAPI development server on port 8151.
 #
 # The server runs with --reload, which means it automatically restarts
 # whenever a Python file in the api/ directory is saved. This is useful
 # during development so you do not have to manually restart after every change.
 #
-# The server will be available at http://127.0.0.1:8000
-# Interactive API docs are at http://127.0.0.1:8000/docs
+# The server will be available at http://127.0.0.1:8151
+# Interactive API docs are at http://127.0.0.1:8151/docs
 #
 # Usage: ./run.sh
 
-uv run uvicorn main:app --reload --port 8000
+uv run uvicorn main:app --reload --port 8151
